ee91bc9c70534ae4ca07616d62ba70c7217355f4
devenamulshikder <devenamulshikder@gmail.com>
2024-09-20 23:03:11 +0600

    payment functionality added

--- src/components/Form/CheckoutForm.jsx
+++ src/components/Form/CheckoutForm.jsx
@@ -1,13 +1,37 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import "./CheckoutForm.css";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useAuth from "../../hooks/useAuth";
 
 const CheckoutForm = ({ closeModal, bookingInfo }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const [clientSecret, setClientSecret] = useState();
+  const [cardError, setCardError] = useState("");
+  const [processing, setProcessing] = useState(false);
+
+  useEffect(() => {
+    // fetch client secret
+    if (bookingInfo?.price && bookingInfo?.price > 1) {
+      getClientSecret({ price: bookingInfo?.price });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bookingInfo?.price]);
+
+  //   get clientSecret
+  const getClientSecret = async (price) => {
+    const { data } = await axiosSecure.post(`/create-payment-intent`, price);
+    console.log("clientSecret from server--->", data);
+    setClientSecret(data.clientSecret);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setProcessing(true);
 
     if (!stripe || !elements) {
       return;
@@ -26,47 +50,80 @@ const CheckoutForm = ({ closeModal, bookingInfo }) => {
 
     if (error) {
       console.log("[error]", error);
+      setCardError(error.message);
+      setProcessing(false);
+      return;
     } else {
       console.log("[PaymentMethod]", paymentMethod);
+      setCardError("");
+    }
+
+    // confirm payment
+    const { error: confirmError, paymentIntent } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email,
+            name: user?.displayName,
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log(confirmError);
+      setCardError(confirmError.message);
+      setProcessing(false);
+      return;
+    }
+
+    if (paymentIntent.status === 'succeeded') {
+      // 1. Create payment info object
+      // 2. save payment info in booking collection (db)
+      // 3. changed room status to booked in db
+
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
+          }}
+        />
 
-      <div className="flex mt-2 justify-around">
-        <button
-          disabled={!stripe}
-          type="button"
-          className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-        >
-          Pay ${bookingInfo?.price}
-        </button>
-        <button
-          type="submit"
-          className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-          onClick={closeModal}
-        >
-          Cancel
-        </button>
-      </div>
-    </form>
+        <div className="flex mt-2 justify-around">
+          <button
+            disabled={!stripe || !clientSecret || processing}
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+          >
+            Pay ${bookingInfo?.price}
+          </button>
+          <button
+            type="submit"
+            className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+      {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+    </>
   );
 };
 
